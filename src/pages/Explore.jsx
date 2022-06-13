01844ea57d4e3c69099b3e65a0eec0791508aaf9
Fadi Zahhar <79freelancer@gmail.com>
2022-06-13 12:26:02 +0300

    adding the create listing and listing pages

--- src/pages/Explore.jsx
+++ src/pages/Explore.jsx
@@ -1,34 +1,35 @@
-import React from 'react'
-import { Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
+import Slider from '../components/Slider'
 import rentCategoryImage from '../assets/jpg/rentCategoryImage.jpg'
-import sellCategoryImage from '../assets/jpg/rentCategoryImage.jpg'
+import sellCategoryImage from '../assets/jpg/sellCategoryImage.jpg'
 
 function Explore() {
   return (
     <div className='explore'>
       <header>
-        <p className="pageHeader">Explore</p>
+        <p className='pageHeader'>Explore</p>
       </header>
 
       <main>
-        {/* slider */}
+        <Slider />
 
-        <p className="exploreCateogryHeading">Categories</p>
-        <div className="exploreCategories">
-          <Link to='/cateogry/rent'>
-            <img src={rentCategoryImage}
-            alt='rent'
-            className='exploreCategoryImg'
+        <p className='exploreCategoryHeading'>Categories</p>
+        <div className='exploreCategories'>
+          <Link to='/category/rent'>
+            <img
+              src={rentCategoryImage}
+              alt='rent'
+              className='exploreCategoryImg'
             />
-            <p className='exploreCategoryName'>Place for rent</p>
+            <p className='exploreCategoryName'>Places for rent</p>
           </Link>
           <Link to='/category/sale'>
             <img
-             src={sellCategoryImage}
-             alt='sale'
-             className='exploreCategoryImg'
-             />
-             <p className='exploreCategoryName'>Places for rent</p>
+              src={sellCategoryImage}
+              alt='sell'
+              className='exploreCategoryImg'
+            />
+            <p className='exploreCategoryName'>Places for sale</p>
           </Link>
         </div>
       </main>
--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,121 +1,126 @@
-import React, {useState} from 'react'
-import { Link, useNavigate } from 'react-router-dom';
-import {getAuth, createUserWithEmailAndPassword, updateProfile} from 'firebase/auth';
-import {setDoc, doc, serverTimestamp} from 'firebase/firestore';
-import { toast } from 'react-toastify';
-import { db } from '../firebase.config';
-import {ReactComponent as ArrowRightIcon} from '../assets/svg/keyboardArrowRightIcon.svg'
-import visibilityIcon from '../assets/svg/visibilityIcon.svg';
-import OAuth from '../components/OAuth';
+import { useState } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
+import { toast } from 'react-toastify'
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  updateProfile,
+} from 'firebase/auth'
+import { setDoc, doc, serverTimestamp } from 'firebase/firestore'
+import { db } from '../firebase.config'
+import OAuth from '../components/OAuth'
+import { ReactComponent as ArrowRightIcon } from '../assets/svg/keyboardArrowRightIcon.svg'
+import visibilityIcon from '../assets/svg/visibilityIcon.svg'
 
 function SignUp() {
-  const [showPassword,setShowPassword] = useState(false);
-  const [formData,setFormData] = useState({
+  const [showPassword, setShowPassword] = useState(false)
+  const [formData, setFormData] = useState({
     name: '',
-    email:'',
+    email: '',
     password: '',
   })
+  const { name, email, password } = formData
 
-  const {name, email, password } = formData;
-
-  const navigate = useNavigate();
+  const navigate = useNavigate()
 
   const onChange = (e) => {
-
     setFormData((prevState) => ({
-     ...prevState,
-     [e.target.id]: e.target.value
+      ...prevState,
+      [e.target.id]: e.target.value,
     }))
   }
 
   const onSubmit = async (e) => {
     e.preventDefault()
+
     try {
       const auth = getAuth()
-      const userCredential = await createUserWithEmailAndPassword
-      (
+
+      const userCredential = await createUserWithEmailAndPassword(
         auth,
         email,
         password
       )
+
       const user = userCredential.user
 
       updateProfile(auth.currentUser, {
-        displayName:name,
+        displayName: name,
       })
 
-      const formDataCopy = {...formData}
+      const formDataCopy = { ...formData }
       delete formDataCopy.password
-      formDataCopy.timestamp = serverTimestamp();
-      await setDoc(doc(db,'users',user.uid), formDataCopy);
+      formDataCopy.timestamp = serverTimestamp()
 
-      navigate('/profile')
-    }
-    catch(error) {
-      toast.error('Something went wrong with the registration');
+      await setDoc(doc(db, 'users', user.uid), formDataCopy)
+
+      navigate('/')
+    } catch (error) {
+      toast.error('Something went wrong with registration')
     }
   }
 
   return (
     <>
-    <div className='pageContainer'>
-      <header>
-        <p className="pageHeader">
-          Welcome Back
-        </p>
-      </header>
+      <div className='pageContainer'>
+        <header>
+          <p className='pageHeader'>Welcome Back!</p>
+        </header>
 
-      <form onSubmit={onSubmit}>
-      <input
-        type='text'
-        className='nameInput'
-        placeholder='Name'
-        id='name'
-        value={name}
-        onChange={onChange}
-        />
-        <input
-        type='email'
-        className='emailInput'
-        placeholder='Email'
-        id='email'
-        value={email}
-        onChange={onChange}
-        />
-        <div className="passwordInputDiv">
-          <input 
-          type={showPassword ? 'text' : 'passowrd'}
-          className='passwordInput'
-          placeholder='Password'
-          id='password'
-          value={password}
-          onChange={onChange}
+        <form onSubmit={onSubmit}>
+          <input
+            type='text'
+            className='nameInput'
+            placeholder='Name'
+            id='name'
+            value={name}
+            onChange={onChange}
           />
-          <img src={visibilityIcon} 
-          alt="show password"
-          className="showPassword"
-          onClick={() => setShowPassword((prevState) => !prevState)}
+          <input
+            type='email'
+            className='emailInput'
+            placeholder='Email'
+            id='email'
+            value={email}
+            onChange={onChange}
           />
-        </div>
-        <Link to='/forgot-password' className='forgotPasswordLink'>
-          Forgot Password
+
+          <div className='passwordInputDiv'>
+            <input
+              type={showPassword ? 'text' : 'password'}
+              className='passwordInput'
+              placeholder='Password'
+              id='password'
+              value={password}
+              onChange={onChange}
+            />
+
+            <img
+              src={visibilityIcon}
+              alt='show password'
+              className='showPassword'
+              onClick={() => setShowPassword((prevState) => !prevState)}
+            />
+          </div>
+
+          <Link to='/forgot-password' className='forgotPasswordLink'>
+            Forgot Password
+          </Link>
+
+          <div className='signUpBar'>
+            <p className='signUpText'>Sign Up</p>
+            <button className='signUpButton'>
+              <ArrowRightIcon fill='#ffffff' width='34px' height='34px' />
+            </button>
+          </div>
+        </form>
+
+        <OAuth />
+
+        <Link to='/sign-in' className='registerLink'>
+          Sign In Instead
         </Link>
-        <div className="signUpBar">
-          <p className="signUpText">
-            Sign In
-          </p>
-          <button className="signInButton">
-            <ArrowRightIcon fill='#ffffff' width='34px'
-            height='34px' />
-          </button>
-        </div>
-      </form>
-
-      <OAuth />
-      <Link to ='/sign-in' className='registerLink'>
-        Sign In Instead
-      </Link>
-    </div>
+      </div>
     </>
   )
 }
