5a21d06c7736b04a76b8223d75c8f03ccfb72f2d
Fadi Zahhar <79freelancer@gmail.com>
2022-06-13 12:25:15 +0300

    finalizing the use Auth Status

--- src/hooks/useAuthStatus.js
+++ src/hooks/useAuthStatus.js
@@ -1,23 +1,32 @@
-import React, {useEffect,useState, useRef} from 'react'
+import { useEffect, useState, useRef } from 'react'
 import { getAuth, onAuthStateChanged } from 'firebase/auth'
 
 export const useAuthStatus = () => {
-    const [loggedIn, seLoggedIn] = useState(false)
-    const [checkingStatus,setCheckingStatus] = useState(true)
-    const isMounted = useRef(true)
+  const [loggedIn, setLoggedIn] = useState(false)
+  const [checkingStatus, setCheckingStatus] = useState(true)
+  const isMounted = useRef(true)
 
-    useEffect(() => {
-        if(isMounted) {
-            const auth = getAuth()
-            onAuthStateChanged(auth,(user) => {
-                if(user) {
-                    seLoggedIn(true)
-                }
-                setCheckingStatus(false);
-            })
+  useEffect(() => {
+    if (isMounted) {
+      const auth = getAuth()
+      onAuthStateChanged(auth, (user) => {
+        if (user) {
+          setLoggedIn(true)
         }
-        
-    },[isMounted])
-  return {loggedIn, checkingStatus}
-  
+        setCheckingStatus(false)
+      })
+    }
+
+    return () => {
+      isMounted.current = false
+    }
+  }, [isMounted])
+
+  return { loggedIn, checkingStatus }
 }
+
+// Protected routes in v6
+// https://stackoverflow.com/questions/65505665/protected-route-with-firebase
+
+// Fix memory leak warning
+// https://stackoverflow.com/questions/59780268/cleanup-memory-leaks-on-an-unmounted-component-in-react-hooks
